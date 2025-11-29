53578d747ca86d9ecd88d1ef5eba2fda80cecc30
PH67602 <hoailinhkk7@gmail.com>
2025-11-29 09:19:29 +0700

    Thêm chức năng sắp xếp

--- CodeTrenLop/CodeTrenLop/Program.c
+++ CodeTrenLop/CodeTrenLop/Program.c
@@ -1,31 +1,123 @@
-// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
-
 #include <stdio.h>
+
+void kiemTraSoNguyen()
+{
+
+		int n;
+		scanf("%d", &n);          
+		int mang[100];
+
+		for (int i = 0; i < n; i++) 
+		{
+			scanf("%d", &mang[i]);   
+		}
+
+		float tong = 0;
+		int count = 0;
+
+		for (int i = 0; i < n; i++) 
+		{
+			if (mang[i] % 3 == 0) 
+			{
+				tong += mang[i];
+				count++;
+			}
+		}
+
+		if (count > 0) 
+		{
+			printf("%.2f\n", tong / count);  
+		}
+		else 
+		{
+			printf("Khong co so nao chia het cho 3\n");
+		}
+}
+
+void sapXepMang()
+{
+	int integerArray[100];
+	int tmp; 
+	int length;
+	printf("Nhap kich thuoc mang: ");
+	scanf("%d", &length);
+	printf("Nhap du lieu cho mang %d phan tu\n", length);
+	//int lenght = sizeof(mangSoNguyen) / sizeof(int);
+	for (int i = 0; i < length ; i++)
+	{
+		printf("mang [%d] = ", i);
+		scanf("%d", &integerArray[i]);
+	}
+	
+
+	printf("Sap xep Giam dan du lieu cho mang %d phan tu\n", length);
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (integerArray[i] < integerArray[i + 1])
+		{
+			tmp = integerArray[i];
+			integerArray[i] = integerArray[i + 1];
+			integerArray[i + 1] = tmp;
+			i = -1;
+		}
+	}
+
+	printf("Du lieu cho mang %d phan tu\n", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d] = %d\n", i, integerArray[i]);
+	}
+}
+
+
+void lapChucNang(int chonChucNang)
+{
+	int tiepTuc = 1;
+	while (tiepTuc == 1)
+	{
+		switch (chonChucNang)
+		{
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			sapXepMang();
+			break;
+		case 3:
+			// ham goi chuc nang 3
+			break;
+		default:
+			printf("Chon sai. Chuc nang hop le [0-3]");
+			break;
+		}
+
+		printf("Tiep tuc thuc hien chuc nang nay? [1=Co | 0=Khong]: ");
+		scanf("%d", &tiepTuc);
+		system("cls");
+	}
+
+}
 
 int main()
 {
-    printf("Hi there!\n");
+	int chonChucNang;
+	do
+	{
+		printf("Menu");
+		printf("\n");
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+		printf("2. Sap xep phan tu mang 1 chieu ");
+		printf("\n");
+		printf("3. TEN chuc nang 3");
+		printf("\n");
+		printf("0. Thoat");
+		printf("\n");
+		printf("Hay chon chuc nang [0-3]: ");
+		scanf("%d", &chonChucNang);
+		lapChucNang(chonChucNang);
+	} while (chonChucNang != 0);
 }
 
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Mo "View" > "Solution Explorer"
-//          de them/quan ly cac files
-//   2. Mo "View" > "Output"
-//          de kiem tra "build output" va "cac thong bao khac"
-//   3. Mo "View" > "Error List"
-//          de xem loi
-//   4. Chon Project > "Add" > "New Item"
-//          de tao moi file code,
-//      hoac Project > "Add" > "Existing Item"
-//          de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//          chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//          trong cua so "Solution Explorer"
-//          bam chuot phai vao "Solution"
-//          chon "Open Folder in File Explorer"
 
 // GV: AnhTT184
